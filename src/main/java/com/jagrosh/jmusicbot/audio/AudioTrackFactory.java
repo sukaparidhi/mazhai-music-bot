6681bbf61ecdd24e608c12afb6c699a2112ad94d
prabah <prabah>
2021-09-08 10:38:02 +0200

    utilizing lava-search instead of youtube API

--- src/main/java/com/jagrosh/jmusicbot/audio/AudioTrackFactory.java
+++ src/main/java/com/jagrosh/jmusicbot/audio/AudioTrackFactory.java
@@ -1,18 +1,16 @@
 package com.jagrosh.jmusicbot.audio;
 
-
-import com.jagrosh.jmusicbot.Bot;
 import com.jagrosh.jmusicbot.audio.data.TrackData;
 import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioTrack;
+import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeSearchProvider;
+import com.sedmelluq.discord.lavaplayer.track.AudioItem;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
 import lombok.extern.slf4j.Slf4j;
 
-
-import java.io.IOException;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -25,14 +23,24 @@ public class AudioTrackFactory {
     }
 
     public AudioTrack getAudioTrack(TrackData trackData) {
-
+        final YoutubeSearchProvider searchProvider = new YoutubeSearchProvider();
+        String identifier = null;
         try {
-            String identifier = Bot.getBot().getYoutubeUtil().getVideoId(trackData.getArtists().get(0) + " " + trackData.getTitle());
+            final StringBuilder query = new StringBuilder();
+            query.append(trackData.getArtists().isEmpty() ? "" : trackData.getArtists().get(0));
+            query.append(trackData.getTitle());
+
+            final AudioItem rawResult =  searchProvider.loadSearchResult(query.toString(),
+                    audioTrackInfo -> new YoutubeAudioTrack(audioTrackInfo, new YoutubeAudioSourceManager()));
+            if (rawResult instanceof AudioPlaylist) {
+                identifier = ((AudioPlaylist) rawResult).getTracks().get(0).getIdentifier();
+            }
             AudioTrackInfo audioTrackInfo = new AudioTrackInfo(
                     trackData.getTitle(),
                     trackData.getArtists().get(0),
                     trackData.getDuration(),
-                    identifier, false,
+                    identifier,
+                    false,
                     trackData.getUrl()
             );
 
